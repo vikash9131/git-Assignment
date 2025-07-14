13c2d655ce0c82386657c16287c6f8b09460b4c8
Vikash Kumar <vikashkumar3165676@gmail.com>
2025-07-15 00:12:56 +0530

    string.js

---
+++ string.js
@@ -0,0 +1,8 @@
+//  How to print length of the string 
+
+function getLength(str){
+    console.log("original string", str);
+    console.log("current length of string = ", str.length);
+}
+
+getLength("Hello world");
