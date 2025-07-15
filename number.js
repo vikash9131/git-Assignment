917adbf19e3614571e21fc48c610e9492f33a8ff
Vikash Kumar <vikashkumar3165676@gmail.com>
2025-07-16 00:20:21 +0530

    number

---
+++ number.js
@@ -0,0 +1,10 @@
+//Explain Usage for parseInt
+function explainParseInt(value){
+    console.log("Orignal value",value);
+    let result = parseInt(value);
+    console.log("After parseInt",result);
+}
+
+explainParseInt("45");
+explainParseInt("45px");
+console.log(parseFloat("4.5"));
