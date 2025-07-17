6daa324b1331a05e111c7086ff0177fb1fb7e488
Vikash Kumar <vikashkumar3165676@gmail.com>
2025-07-18 00:35:50 +0530

    commit again

--- date.js
+++ date.js
@@ -1,12 +1,12 @@
-// const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-// const currentDate = new Date();
-// console.log(days[currentDate.getDay()]);
-// console.log(currentDate.getDate());
-// console.log(currentDate.getMonth()+1);
-// console.log(currentDate.getFullYear());
-// console.log(currentDate.getMinutes());
-// console.log(currentDate.getHours());
+const currentDate = new Date();
+console.log(days[currentDate.getDay()]);
+console.log(currentDate.getDate());
+console.log(currentDate.getMonth()+1);
+console.log(currentDate.getFullYear());
+console.log(currentDate.getMinutes());
+console.log(currentDate.getHours());
 
 
 function calculateSum(){
